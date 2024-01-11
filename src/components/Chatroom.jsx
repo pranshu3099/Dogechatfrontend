da92718a1e53666c03d8a1f587f9be864815a0af
pranshu <srivastavapranshu2@gmail.com>
2024-01-12 02:02:25 +0530

    handling chats

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,9 +1,16 @@
 import Doge from "../icons/Doge.jpg";
 import sendbutton from "../icons/send-alt-1-svgrepo-com.svg";
 import { Textarea } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import React from "react";
-const Chatroom = ({ sendMessage, setMessagesarr, messagearr }) => {
+const Chatroom = ({
+  sendMessage,
+  setMessagesarr,
+  messagearr,
+  addToLocalStorage,
+  getChats,
+  open,
+}) => {
   const [message, setMessage] = useState("");
   const handleMessage = (message) => {
     setMessage(message);
@@ -12,9 +19,11 @@ const Chatroom = ({ sendMessage, setMessagesarr, messagearr }) => {
   const handleSendMessage = () => {
     if (message === "" || checkForSpaces()) return;
     else {
+      let user_new_message = { message: message, role: "user" };
       setMessagesarr((prev) => {
-        return [...prev, { message: message, role: "user" }];
+        return [...prev, user_new_message];
       });
+      addToLocalStorage(user_new_message);
       sendMessage(message);
       setMessage("");
     }
@@ -37,44 +46,53 @@ const Chatroom = ({ sendMessage, setMessagesarr, messagearr }) => {
 
   return (
     <>
-      <div className="chatroom-container">
-        <div className="chatroom-sub-container">
-          <div className="chatroom-name">
-            <img src={Doge} style={{ width: "50px" }} />
-            <p>cheems</p>
+      {open ? (
+        <div className="chatroom-container">
+          <div className="chatroom-sub-container">
+            <div className="chatroom-name">
+              <img src={Doge} style={{ width: "50px" }} />
+              <p>cheems</p>
+            </div>
+            <div className="main-chats">
+              {<Addelement messagearr={messagearr} getChats={getChats} />}
+            </div>
           </div>
-          <div className="main-chats">
-            {<Addelement messagearr={messagearr} />}
+          <div className="chatroom-inputbox">
+            <Textarea
+              placeholder="yomr memessage"
+              className="chat-input"
+              value={message}
+              onChange={(e) => {
+                handleMessage(e.target.value);
+              }}
+              onKeyPress={handleKeyPress}
+            ></Textarea>
+            <img
+              src={sendbutton}
+              alt=""
+              className="send-btn"
+              onClick={handleSendMessage}
+            />
           </div>
         </div>
-        <div className="chatroom-inputbox">
-          <Textarea
-            placeholder="yomr memessage"
-            className="chat-input"
-            value={message}
-            onChange={(e) => {
-              handleMessage(e.target.value);
-            }}
-            onKeyPress={handleKeyPress}
-          ></Textarea>
-          <img
-            src={sendbutton}
-            alt=""
-            className="send-btn"
-            onClick={handleSendMessage}
-          />
-        </div>
-      </div>
+      ) : (
+        <h1></h1>
+      )}
     </>
   );
 };
 
-const Addelement = React.memo(({ messagearr }) => {
-  console.log(messagearr);
-  return messagearr.length
-    ? messagearr.reverse().map((msg, index) => {
+const Addelement = React.memo(({ messagearr, getChats }) => {
+  let arr = [];
+  if (messagearr.length) {
+    messagearr = [...getChats, ...messagearr];
+  }
+  arr = messagearr?.length ? messagearr : getChats;
+
+  return arr.length
+    ? arr.map((msg, index) => {
         return (
-          <div className="chats">
+          <div className="chats" key={index}>
             <p
               className={msg?.role === "user" ? "user-message" : "doge-message"}
               key={index}
--- src/components/DogeList.jsx
+++ src/components/DogeList.jsx
@@ -1,9 +1,12 @@
 import Doge from "../icons/Doge.jpg";
-const DogeList = () => {
+const DogeList = ({ handleOpen }) => {
+  const handleOpenChats = () => {
+    handleOpen();
+  };
   return (
     <>
       <div className="chat-container">
-        <div className="dog-container">
+        <div className="dog-container" onClick={handleOpenChats}>
           <img src={Doge} style={{ width: "50px" }} />
           <div className="doge-name">
             <p>cheems</p>
