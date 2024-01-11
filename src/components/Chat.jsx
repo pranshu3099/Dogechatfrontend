37b042ede0963d914beef483d798c8c11b82faf6
pranshu <srivastavapranshu2@gmail.com>
2024-01-12 02:01:17 +0530

    add chats to localstorage

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,18 +7,34 @@ import Chatroom from "./Chatroom";
 import { useLocation } from "react-router-dom";
 export const Chat = () => {
   const [messagearr, setMessagesarr] = useState([]);
+  const [getChats, setGetchats] = useState([]);
+  const [open, setOpen] = useState(false);
   const socket = io("http://localhost:3000", { path: "/socket.io" });
   const location = useLocation();
   // console.log(location);
   // let profilepicture = location?.state?.data?.url[0].path;
+
+  const addToLocalStorage = (new_message) => {
+    let chats = JSON.parse(localStorage.getItem("chats"));
+    if (chats) {
+      chats = [...chats, new_message];
+      localStorage.setItem("chats", JSON.stringify(chats));
+    } else {
+      let chats = [];
+      chats.push(new_message);
+      localStorage.setItem("chats", JSON.stringify(chats));
+    }
+  };
   useEffect(() => {
-    socket.on("chat message", (msg, type = null) => {
-      console.log(type);
+    socket.on("chat_message", (msg, type = null) => {
       console.log("Recieved message", msg);
+      let doge_new_message = { message: msg, role: "doge" };
       if (type === "doge") {
         setMessagesarr((prev) => {
-          return [...prev, { message: msg, role: "doge" }];
+          return [...prev, doge_new_message];
         });
+        console.log(messagearr, "chatss");
+        addToLocalStorage(doge_new_message);
       }
     });
 
@@ -27,17 +43,31 @@ export const Chat = () => {
     };
   }, []);
 
+  useEffect(() => {
+    let chats = JSON.parse(localStorage.getItem("chats"));
+    if (chats) {
+      setGetchats(chats);
+    }
+  }, []);
+
   const sendMessage = (message) => {
-    socket.emit("chat message", message, "user");
+    socket.emit("chat_message", message, "user");
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
   };
 
   return (
     <div className="chat-main-container">
-      <DogeList />
+      <DogeList handleOpen={handleOpen} />
       <Chatroom
         sendMessage={sendMessage}
         setMessagesarr={setMessagesarr}
         messagearr={messagearr}
+        addToLocalStorage={addToLocalStorage}
+        getChats={getChats}
+        open={open}
       />
     </div>
   );
