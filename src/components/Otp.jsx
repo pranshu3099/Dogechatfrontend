46a0aea8d5474ab94ef176e561b7e4819617a53f
pranshu <srivastavapranshu2@gmail.com>
2024-02-04 15:21:08 +0530

    Otp page

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useReducer, useState, useRef, useEffect } from "react";
 import { Input, Button } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
@@ -6,29 +6,53 @@ import { Navigate } from "react-router-dom";
 const otpReducer = (state, action) => {
   switch (action.type) {
     case "input1":
-      return { ...state, input1: action.input1 };
+      return {
+        ...state,
+        input1: action.input1.slice(0, 1),
+        input2: action.input1.length > 1 ? action.input1.slice(1, 2) : "",
+      };
     case "input2":
-      return { ...state, input2: action.input2 };
+      return {
+        ...state,
+        input2: action.input2.slice(0, 1),
+        input3: action.input2.length > 1 ? action.input2.slice(1, 2) : "",
+      };
     case "input3":
-      return { ...state, input3: action.input3 };
+      return {
+        ...state,
+        input3: action.input3.slice(0, 1),
+        input4: action.input3.length > 1 ? action.input3.slice(1, 2) : "",
+      };
     case "input4":
-      return { ...state, input4: action.input4 };
+      return {
+        ...state,
+        input4: action.input4.slice(0, 1),
+      };
     default:
-      throw new Error("type not matched");
+      return state;
   }
 };
-
 const VerifyOtp = () => {
   const location = useLocation();
   const verificationInfo = location.state;
   let { mobileNumber } = verificationInfo;
   const [auth, setAuth] = useState(false);
+  const [focusedInput, setFocusedInput] = useState(0);
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
+  const inputRef4 = useRef(null);
   const [data, dispatch] = useReducer(otpReducer, {
     input1: "",
     input2: "",
     input3: "",
     input4: "",
   });
+  // console.log(data);
+
+  useEffect(() => {
+    inputRef1.current?.focus();
+  }, []);
 
   const verifyuser = () => {
     let otp = data?.input1 + data?.input2 + data?.input3 + data?.input4;
@@ -45,12 +69,28 @@ const VerifyOtp = () => {
       .catch((err) => console.log(err));
   };
 
+  const movetoNextInput = (e, eve) => {
+    const inputRefs = [inputRef1, inputRef2, inputRef3, inputRef4];
+    // console.log(focusedInput);
+    if (focusedInput === 0 && e.target.value === "") {
+      return;
+    } else if (e.target.value === "") {
+      inputRefs[focusedInput - 1].current.disabled = false;
+      inputRefs[focusedInput - 1].current.focus();
+      setFocusedInput((prev) => prev - 1);
+    } else if (focusedInput < inputRefs.length - 1) {
+      inputRefs[focusedInput + 1].current.focus();
+      inputRefs[focusedInput].current.disabled = true;
+      setFocusedInput((prev) => prev + 1);
+    }
+  };
+
   return (
     <>
       <div className="signup-main-container">
         <div className="signup">
           <h1 className="signup-h1" style={{ color: "white" }}>
-            enter the otp send to your mobile number
+            enter the otp send to your email
           </h1>
         </div>
 
@@ -61,6 +101,7 @@ const VerifyOtp = () => {
                 type="number"
                 width={"50px"}
                 margin={5}
+                ref={inputRef1}
                 name="input1"
                 value={data?.input1}
                 style={{ color: "white" }}
@@ -69,14 +110,21 @@ const VerifyOtp = () => {
                     ...data,
                     input1: e.target.value,
                     type: "input1",
-                  });
+                  }),
+                    movetoNextInput(e);
                 }}
-                minLength="1"
+                onKeyUp={(e) => {
+                  // console.log("value", e.target.value);
+                  if (e.key === "Backspace" && e.target.value === "") {
+                    movetoNextInput(e);
+                  }
+                }}
               />
               <Input
                 type="number"
                 width={"50px"}
                 margin={5}
+                ref={inputRef2}
                 name="input2"
                 value={data?.input2}
                 style={{ color: "white" }}
@@ -85,13 +133,21 @@ const VerifyOtp = () => {
                     ...data,
                     input2: e.target.value,
                     type: "input2",
-                  });
+                  }),
+                    movetoNextInput(e);
+                }}
+                onKeyUp={(e) => {
+                  // console.log("value", e.target.value);
+                  if (e.key === "Backspace" && e.target.value === "") {
+                    movetoNextInput(e);
+                  }
                 }}
               />
               <Input
                 type="number"
                 width={"50px"}
                 margin={5}
+                ref={inputRef3}
                 name="input3"
                 value={data?.input3}
                 style={{ color: "white" }}
@@ -100,13 +156,21 @@ const VerifyOtp = () => {
                     ...data,
                     input3: e.target.value,
                     type: "input3",
-                  });
+                  }),
+                    movetoNextInput(e);
+                }}
+                onKeyUp={(e) => {
+                  // console.log("value", e.target.value);
+                  if (e.key === "Backspace" && e.target.value === "") {
+                    movetoNextInput(e);
+                  }
                 }}
               />
               <Input
                 type="number"
                 width={"50px"}
                 margin={5}
+                ref={inputRef4}
                 name="input4"
                 value={data?.input4}
                 style={{ color: "white" }}
@@ -115,7 +179,15 @@ const VerifyOtp = () => {
                     ...data,
                     input4: e.target.value,
                     type: "input4",
-                  });
+                  }),
+                    movetoNextInput(e);
+                }}
+                onKeyUp={(e) => {
+                  // console.log("value", e.target.value);
+
+                  if (e.key === "Backspace" && e.target.value === "") {
+                    movetoNextInput(e);
+                  }
                 }}
               />
 
