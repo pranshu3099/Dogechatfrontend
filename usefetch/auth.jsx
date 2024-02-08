78ba0eadc569c951fa470ad79e58556ec8f2122f
pranshu <srivastavapranshu2@gmail.com>
2024-02-08 16:03:49 +0530

    auth page

--- usefetch/auth.jsx
+++ usefetch/auth.jsx
@@ -1,21 +1,18 @@
 import { useEffect, useState } from "react";
-
+const react_api_url = import.meta.env.VITE_REACT_APP_API_URL;
 const useFetch = () => {
   const [data, setData] = useState(false);
   const [err, setError] = useState("");
   useEffect(() => {
     const checkValidity = async () => {
       try {
-        const res = await fetch(
-          `http://localhost:3000/dogechat/checkvalidity`,
-          {
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            credentials: "include",
-          }
-        );
+        const res = await fetch(`${react_api_url}/dogechat/checkvalidity`, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
         const data = await res.json();
         if (data?.success) {
           setData(data);
