f47356239be390c0d6a36cfc3a08980000c184f0
pranshu <srivastavapranshu2@gmail.com>
2024-02-19 17:34:15 +0530

    added TokenValidity function

---
+++ tokenvalidity/auth.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+const react_api_url = import.meta.env.VITE_REACT_APP_API_URL;
+const TokenValidity = () => {
+  const [data, setData] = useState(false);
+  const [err, setError] = useState("");
+  useEffect(() => {
+    const checkValidity = async () => {
+      try {
+        const res = await fetch(`${react_api_url}/dogechat/checkvalidity`, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        const data = await res.json();
+        if (data) {
+          setData(data);
+        }
+      } catch (err) {
+        setError(err);
+        console.log(err);
+      }
+    };
+    checkValidity();
+  }, []);
+  return { data, err };
+};
+
+export default TokenValidity;
